1dd0fa2f04d887cfb8031079545f1cfe288a66fe
Artdecide <moscofff@gmail.com>
2023-06-20 16:41:41 +0300

    создали 2 файла Card and  ConcentrationGame. Первый файл содержит структуру карточек их свойства и инициализатор id. Второй файл это класс который содержит в себе массив из карточек и метод бизнес логики игры.

---
+++ Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Александр Кулаков on 20.06.2023.
+//
+
+import Foundation
+
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int 
+}
---
+++ ConcentrationGame.swift
@@ -0,0 +1,18 @@
+//
+//  ConcentrationGame.swift
+//  Concentration
+//
+//  Created by Александр Кулаков on 20.06.2023.
+//
+
+import Foundation
+
+
+class ConcentrationGame {
+    var cards = [Card]()
+    
+    //Алгоритм как должна играть игра
+    func chooseCard(at index: Int) {
+        
+    }
+}
