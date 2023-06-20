b8ff3d61d4da0c2dc0220b0a8ca6e4116ee1e6b8
Artdecide <moscofff@gmail.com>
2023-06-20 17:39:10 +0300

    Сделали идентификатор для карточек  в структуре Card

--- Card.swift
+++ Card.swift
@@ -11,5 +11,15 @@ import Foundation
 struct Card {
     var isFaceUp = false
     var isMatched = false
-    var identifier: Int 
+    var identifier: Int
+    
+    static var identifierNumber = 0
+    static func identifierGeneration() -> Int {
+        identifierNumber += 1
+        return identifierNumber
+    }
+    
+    init() {
+        self.identifier = Card.identifierGeneration()
+    }
 }
