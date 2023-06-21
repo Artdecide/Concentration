71950c2c988908da0b0d8f6953974211b2d45b68
Artdecide <moscofff@gmail.com>
2023-06-21 13:36:00 +0300

    написали бизнес логику в классе ConcentrationGame

--- ConcentrationGame.swift
+++ ConcentrationGame.swift
@@ -10,10 +10,26 @@ import Foundation
 
 class ConcentrationGame {
     var cards = [Card]()
+    var indexOfOneOnlyFaceUpCard: Int?
     
     //Алгоритм как должна играть игра
     func chooseCard(at index: Int) {
-        
+        if !cards[index].isMatched {
+            if let matchingIndex = indexOfOneOnlyFaceUpCard, matchingIndex != index {
+                if cards[matchingIndex].identifier == cards[index].identifier {
+                    cards[matchingIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneOnlyFaceUpCard = nil
+            } else {
+                for flipDown in cards.indices {
+                    cards[flipDown].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneOnlyFaceUpCard = index 
+            }
+        }
     }
     
     
