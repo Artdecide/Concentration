24b47be7651c64afcaa43d60cf6a90e907f20342
Artdecide <moscofff@gmail.com>
2023-06-21 14:13:26 +0300

    добавили новый метод выбора эмодзи, через словарь и написали эту функцию, создав рандом и исключив возможность  появления одинаковых эмодзи

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -21,14 +21,24 @@ class ViewController: UIViewController {
     
    
 
-    var emojiCollection = ["🍏", "🍎", "🍏", "🍎"]
+    var emojiCollection = ["🍏", "🍎", "🍐", "🍊", "🍋", "🍓", "🍈", "🥥", "🍅", "🫐", "🍑", "🥝"]
+    var emojiDictionary = [Int:String]()
+    
+    func emojiIdentifier(for card: Card) -> String {
+        if emojiDictionary[card.identifier] == nil {
+            let randomEmoji = Int(arc4random_uniform(UInt32(emojiCollection.count)))
+            emojiDictionary[card.identifier] = emojiCollection.remove(at: randomEmoji)
+        }
+        return emojiDictionary[card.identifier] ?? "?"
+    }
+    
     
     func updateViewFromModel() {
         for index in buttonCollection.indices {
             let button = buttonCollection[index]
             let card = game.cards[index]
             if card.isFaceUp {
-                button.setTitle(emoji, for: .normal)
+                button.setTitle(emojiIdentifier(for: card), for: .normal)
                 button.backgroundColor = #colorLiteral(red: 0.9999999404, green: 0.9999999404, blue: 1, alpha: 1)
             } else {
                 button.setTitle("", for: .normal)
